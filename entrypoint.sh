fb6b1283d651301119b9cf9943f86c0ad9f0dc6f
Maksym Bilenko <Maksym_Bilenko@epam.com>
2016-03-28 18:04:07 +0300

    Move dir creation to the git puller

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
-mkdir $APP_HOME
+
 case $1 in
 	'')
+	#initial sleep for puller
+	sleep 30
 	cd $APP_HOME
 	bundle install --system
 	rerun "${RERUN_OPTS} ${APP_MAIN}"
 	;;
 
 	'git-puller')
+	mkdir $APP_HOME
 	cd $APP_HOME
 	mkdir ~/.ssh
 	chmod 700 ~/.ssh
