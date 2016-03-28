b81ca079db097d77c5d2673d558419328b7397f8
Maksym Bilenko <Maksym_Bilenko@epam.com>
2016-03-28 17:54:36 +0300

    Update entrypoint for git-puller

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,15 @@ case $1 in
 	cd $APP_HOME
 	mkdir ~/.ssh
 	chmod 700 ~/.ssh
-	ln -s /run/secrets/git/id.rsa ~/.ssh/id_rsa
+	cat /run/secrets/git/id.rsa > ~/.ssh/id_rsa
+	chmod 600 ~/.ssh/id_rsa
 	SERVER=${2%:*}
 	SERVER=${SERVER#*@}
 	ssh-keyscan $SERVER >> ~/.ssh/known_hosts
+	REPO=${2%.git}
+	REPO=${REPO#*/}
 	git clone $2
+	cd $REPO
 	while true; do
 		git pull
 		sleep 15
